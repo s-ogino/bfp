3cb65fedf14423d390a0cd80d10ee942dcc7a376
s-ogino <s-ogino@bfp.co.jp>
2019-10-07 22:04:38 +0900

    lengthをnumに修正

--- fizzBuzz/ogino.js
+++ fizzBuzz/ogino.js
@@ -1,6 +1,6 @@
-var length = 50;
+var num = 50;
 
-for (let i = 1; i <= length; i++) {
+for (let i = 1; i <= num; i++) {
     var fizz = (i % 3 == 0) ? "Fizz" : "";
     var buzz = (i % 5 == 0) ? "Buzz" : "";
     var result = (fizz || buzz) ? fizz + buzz : i;
